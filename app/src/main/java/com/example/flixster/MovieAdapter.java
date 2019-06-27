b9500087db51e59a7d0dc16bea2eccdd7bdbe647
Marta Garcia Ferreiro <martagferreiro@fb.com>
2019-06-27 16:41:12 -0700

    initial commit

---
+++ app/src/main/java/com/example/flixster/MovieAdapter.java
@@ -0,0 +1,101 @@
+package com.example.flixster;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.models.Config;
+import com.example.flixster.models.Movie;
+
+import java.util.ArrayList;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    ArrayList<Movie> movies;
+    Config config;
+    Context context;
+
+    public MovieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    // creates and inflates a new view
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View movieView = inflater.inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    // binds an inflated view to a new item
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+
+        Movie movie = movies.get(i);
+
+        // determine current orientation
+
+        boolean isPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
+        String imageUrl = null;
+
+        if (isPortrait) {
+            imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        } else {
+            imageUrl = config.getImageUrl(config.getPosterSize(), movie.getBackdropPath());
+        }
+
+        // get the correct placeholder and imageview
+        int placeholderId = isPortrait ? R.drawable.flicks_movie_placeholder : R.drawable.flicks_backdrop_placeholder;
+        ImageView imageView = isPortrait ? viewHolder.ivPosterImage : viewHolder.ivBackdropImage;
+
+        viewHolder.tvTitle.setText(movie.getTitle());
+        viewHolder.tvOverview.setText(movie.getOverview());
+        Glide.with(context)
+                .load(imageUrl)
+                .bitmapTransform(new RoundedCornersTransformation(context, 20, 0))
+                .placeholder(placeholderId)
+                .error(placeholderId)
+                .into(imageView);
+    }
+
+    // returns the total number of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    // create the viewholder as static inner class
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivPosterImage;
+        ImageView ivBackdropImage;
+        TextView tvTitle;
+        TextView tvOverview;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ivPosterImage = itemView.findViewById(R.id.ivPosterImage);
+            ivBackdropImage = itemView.findViewById(R.id.ivBackdropImage);
+            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/flixster/models/Config.java
@@ -0,0 +1,35 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Config {
+    String imageBaseUrl;
+    String posterSize;
+    String backdropSize;
+
+    public Config(JSONObject object) throws JSONException {
+        JSONObject images = object.getJSONObject("images");
+        imageBaseUrl = images.getString("secure_base_url");
+        JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
+        posterSize = posterSizeOptions.optString(3, "w342");
+        JSONArray backdropSizeOptions = images.getJSONArray("backdrop_sizes");
+        backdropSize = backdropSizeOptions.optString(1, "w780");
+    }
+
+    public String getBackdropSize() { return backdropSize; }
+
+    // helper method for creating urls
+    public String getImageUrl(String size, String path) {
+        return String.format("%s%s%s", imageBaseUrl, size, path);
+    }
+
+    public String getImageBaseUrl() {
+        return imageBaseUrl;
+    }
+
+    public String getPosterSize() {
+        return posterSize;
+    }
+}
